7fdf7c0293c3d24a291df9bf828a24a3525d77e7
vsbdn <v.boldurean@gmail.com>
2021-04-17 16:20:35 +0100

    update comma

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  watch: true,
+  watchOptions: {
+    ignored: '/node_modules',
+  },
   module: {
     rules: [
       {
@@ -11,11 +21,15 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'template.html',
+      template: 'index.html',
     }),
   ],
 };
