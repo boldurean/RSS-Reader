0056b8620b754af93ba2be112e7741b970bfeea2
vsbdn <v.boldurean@gmail.com>
2021-04-29 12:12:02 +0100

    add: bootstrap

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ import initAutoupdate from './autoupdate.js';
 import resources from './locales/resources.js';
 import { getRssData, extractFeed, extractPosts } from './utils.js';
 import validate from './validate.js';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap';
 import {
   renderFeedback,
   renderPosts,
